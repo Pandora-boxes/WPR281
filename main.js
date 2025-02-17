599a985a752bb34b7b5ef9eab7d77d9d638feeed
Bodebate <601463@student.belgiumcampus.ac.za>
2025-02-18 00:54:38 +0200

    made the validation check a function and more

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let loggedInUser = -1;
 let userList=[];
+
 let Account = {
     userName : "",
     userPassword :"",
@@ -32,7 +33,6 @@ function PopulateUser(fUsername,fUserPassword,fFirstName,fLastName,fHeight,fWeig
     newUser.datejoined = new Date()
     newUser.weightLog.shift();
     newUser.weightLog.push(newUser.weight,newUser.datejoined);
-    newUser.weightLog.push(newUser.weight,newUser.datejoined);
     return newUser
 }
 
@@ -48,34 +48,61 @@ function logExercise(fname,fDuration,fCBurnt){
         fCurrentUser.exercisesComplete.push(fObject);
 }
 
+function stringValidation(checkVariable,checkUnique,checkMidSpace){
+    let bValidation = true
+    if (checkVariable == null){bValidation=false}else{
 
+        checkVariable = checkVariable.trim();
+
+        if (checkVariable == "" || (verifiedUserName.indexOf(" ")!=-1 && checkMidSpace)){bValidation=false}
+        else{
+            let checkinglist = userList.filter(User => User.userName==verifiedUserName);
+
+            if (checkinglist.length>0 && checkUnique){bValidation=false}
+        }     
+    }
+    return bValidation
+}
 
 window.addEventListener('load', app);
 
       function app() {
         const JoinButton =document.getElementById('JoinButton');
             function makeUser(){
+                //ensuring validity of usename entered 
                 let bValid=true;
-                let verifiedUserName =prompt("please enter a username");
+                let validUserName =prompt("Please enter a username");
 
-                do{
-                    bValid=true;
-                    let checkinglist = userList.filter(User => User.userName==verifiedUserName);
+                while(stringValidation(validUserName,true,true)){
+                    validUserName = prompt('Username invalid or in use\nPlease enter a new username:');
+                }
 
-                    if (checkinglist.length>0){
-                        bValid=false
-                        verifiedUserName =prompt("username already in use\nplease enter a username");
-                    };
-                }while(!bValid)
+                validUserName=validUserName.trim();
+
+                let validPassword = prompt("Please enter a password");
+                while(stringValidation(validPassword),false,true){
+                    validPassword = prompt('Password invalid\nPlease enter a new password:');
+                }
+                validPassword = validPassword.trim();
 
 
+                let validFirstName = prompt('Please enter your first name');
+                while(stringValidation(validFirstName,false,false)){
+                    validFirstName = prompt('Name invalid\nPlease enter a new first name:');
+                }
+                validFirstName =validFirstName.trim();
 
-
+                let validSurname = prompt("Please enter your surname");
+                while(stringValidation(validSurname,false,false)){
+                    validSurname = prompt('Name invalid\nPlease enter a new Surnamename:');
+                }
+                validSurname=validSurname.trim();
+                
                 userList.push(
                 PopulateUser(verifiedUserName,
-                prompt('enter a password'),
-                prompt('enter a first name'),
-                prompt('enter a last name'),
+                validPassword,
+                validFirstName,
+                validSurname,
                 prompt('enter a height'),
                 prompt('enter a weight'),
                 prompt('enter an age')))
