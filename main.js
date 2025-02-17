982d92d5e9607c7823cb693ff927a196f32ecd97
Bodebate <601463@student.belgiumcampus.ac.za>
2025-02-18 01:00:35 +0200

    moved functions for better accessability

--- main.js
+++ main.js
@@ -63,51 +63,49 @@ function stringValidation(checkVariable,checkUnique,checkMidSpace){
     }
     return bValidation
 }
+function makeUser(){
+    
+    //ensuring validity of usename entered 
+    let bValid=true;
+    let validUserName =prompt("Please enter a username");
+
+    while(stringValidation(validUserName,true,true)){
+        validUserName = prompt('Username invalid or in use\nPlease enter a new username:');
+    }
+
+    validUserName=validUserName.trim();
+
+    let validPassword = prompt("Please enter a password");
+    while(stringValidation(validPassword),false,true){
+        validPassword = prompt('Password invalid\nPlease enter a new password:');
+    }
+    validPassword = validPassword.trim();
+
+
+    let validFirstName = prompt('Please enter your first name');
+    while(stringValidation(validFirstName,false,false)){
+        validFirstName = prompt('Name invalid\nPlease enter a new first name:');
+    }
+    validFirstName =validFirstName.trim();
+
+    let validSurname = prompt("Please enter your surname");
+    while(stringValidation(validSurname,false,false)){
+        validSurname = prompt('Name invalid\nPlease enter a new Surnamename:');
+    }
+    validSurname=validSurname.trim();
+    
+    userList.push(PopulateUser(verifiedUserName,validPassword,validFirstName,validSurname,
+    prompt('enter a height'),
+    prompt('enter a weight'),
+    prompt('enter an age')))
+
+    loggedInUser = userList.length-1;
+    console.log(userList[loggedInUser])
+}
 
 window.addEventListener('load', app);
 
       function app() {
-        const JoinButton =document.getElementById('JoinButton');
-            function makeUser(){
-                //ensuring validity of usename entered 
-                let bValid=true;
-                let validUserName =prompt("Please enter a username");
-
-                while(stringValidation(validUserName,true,true)){
-                    validUserName = prompt('Username invalid or in use\nPlease enter a new username:');
-                }
-
-                validUserName=validUserName.trim();
-
-                let validPassword = prompt("Please enter a password");
-                while(stringValidation(validPassword),false,true){
-                    validPassword = prompt('Password invalid\nPlease enter a new password:');
-                }
-                validPassword = validPassword.trim();
-
-
-                let validFirstName = prompt('Please enter your first name');
-                while(stringValidation(validFirstName,false,false)){
-                    validFirstName = prompt('Name invalid\nPlease enter a new first name:');
-                }
-                validFirstName =validFirstName.trim();
-
-                let validSurname = prompt("Please enter your surname");
-                while(stringValidation(validSurname,false,false)){
-                    validSurname = prompt('Name invalid\nPlease enter a new Surnamename:');
-                }
-                validSurname=validSurname.trim();
-                
-                userList.push(
-                PopulateUser(verifiedUserName,
-                validPassword,
-                validFirstName,
-                validSurname,
-                prompt('enter a height'),
-                prompt('enter a weight'),
-                prompt('enter an age')))
-                loggedInUser = userList.length-1;
-                console.log(userList[loggedInUser])
-            }
+            const JoinButton =document.getElementById('JoinButton');
             JoinButton.addEventListener('click',makeUser);
-}
+        }
