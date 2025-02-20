fe3f0dce00ffeead10076fcec514bc3a323184ed
Dylan <dylan.james.spurrier@gmail.com>
2025-02-20 13:53:40 +0200

    fixed file location issues with landing images

--- script_L5.js
+++ script_L5.js
@@ -3,6 +3,6 @@
 document.getElementById('detailsForm2').addEventListener('submit', function(e) {
     e.preventDefault();
     if(this.checkValidity()) {
-        window.location.href = 'home.html';
+        window.location.href = 'mainbone.html';
     }
 });
