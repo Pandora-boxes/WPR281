74ce9e29d64d80d463aaa4ac1079b5c7bb4186a9
Bodebate <601463@student.belgiumcampus.ac.za>
2025-02-21 14:52:26 +0200

    good luck have fun

--- main.js
+++ main.js
@@ -1,108 +1,3 @@
-/*const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-links");
-const menuBtnIcon = menuBtn.querySelector("i");
-
-menuBtn.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open");
-
-  const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-});
-
-navLinks.addEventListener("click", (e) => {
-  navLinks.classList.remove("open");
-  menuBtnIcon.setAttribute("class", "ri-menu-line");
-});
-
-
-// defining animation setting
-const scrollRevealOption = {
-  origin: "bottom", //animations start from bottom
-  distance: "50px", //moves 50px
-  duration: 1000,
-};
-
-//applies scrollreveal animation to head
-ScrollReveal().reveal(".header__image img", {
-  ...scrollRevealOption,
-  origin: "right",//overrides, slide in from the right
-});
-ScrollReveal().reveal(".header__content h1", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-ScrollReveal().reveal(".header__content h2", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-ScrollReveal().reveal(".header__content p", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
-ScrollReveal().reveal(".header__btn", {
-  ...scrollRevealOption,
-  delay: 2000,
-});
-
-ScrollReveal().reveal(".jump__image img", {
-  ...scrollRevealOption,
-  origin: "left",
-});
-ScrollReveal().reveal(".jump__content .section__header", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-ScrollReveal().reveal(".jump__content p", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-ScrollReveal().reveal(".jump__btn", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
-
-ScrollReveal().reveal(".service__card", {
-  duration: 1000,
-  interval: 500,
-});
-
-ScrollReveal().reveal(".inspire__content .section__header", {
-  ...scrollRevealOption,
-});
-ScrollReveal().reveal(".inspire__content p", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-
-ScrollReveal().reveal(".mentor__card", {
-  ...scrollRevealOption,
-  interval: 500,
-});
-
-
-
-//displays random quotes
-const quotes = [
-    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-    "Believe you can and you're halfway there. - Theodore Roosevelt",
-    "Act as if what you do makes a difference. It does. - William James",
-    "It always seems impossible until it’s done. - Nelson Mandela",
-    "Push through the pain. Giving up hurts more.” — Vegeta, Dragon Ball Z",
-    "Hard work is worthless for those that don’t believe in themselves.” — Naruto Uzumaki, Naruto",
-    "It’s not dying that frightens us. It’s living without ever having done our best.” — The Elder, Castlevania",
-    "Whether You Win Or Lose...You Can Always Come Out Ahead By Learning From The Experience. - All Might"
-]
-
-function getRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  document.getElementById("generateQuote").innerText = quotes[randomIndex];//changes innertext to the actual text
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  getRandomQuote(); // Show a random quote on page load
-  setInterval(getRandomQuote, 8000);
-});
-*/
 let usertemp = {};
 let loggedInUser = -1;
 let userList=[];
@@ -300,6 +195,7 @@ function loadLanding2(){
         <h1>Choose your body type</h1>
 
         <div class="body-type-options">
+
             <a class="landing2-body-card">
                 <div class="option">
                     <img src="/Slim.webp" alt="Slim Body Type">
@@ -343,7 +239,7 @@ function loadLanding2(){
     documentHead.innerHTML=newPageHeader;
     documentBody.innerHTML = newpagebody;
 
-    let landing3Button = documentBody.querySelector('.landing2-body-card');
+    let landing3Button = documentBody.querySelector('.option');
     let loginButton = documentBody.querySelector('#Login');
     let logoButton = documentBody.querySelector('#logo');
 
@@ -615,7 +511,7 @@ function loadMainBone(){
         <ul class="nav__links" id="nav-links">
           <!-- these hrefs we dont have to change, it allows for automatic
                scrolling to these sections on the home page -->
-          <li><a id="home">Home</a></li>
+          <li><a href="#home">Home</a></li>
           <li><a href="#jump">Recent</a></li>
           <li><a href="#service">Services</a></li>
           <li><a id="logOut">LogOut</a></li>
@@ -832,8 +728,7 @@ function loadMainBone(){
     href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css"
     rel="stylesheet"/>
 
-     <!-- allows for scroll animation -->
-    <script src="https://unpkg.com/scrollreveal"></script>
+     
     <link rel="stylesheet" href="mainstyle.css" />
     <title>web project</title>`;
 
@@ -848,6 +743,111 @@ function loadMainBone(){
         loadIndex();
     })
 
+    //mainbone dedicated js
+    const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+
+// defining animation setting
+const scrollRevealOption = {
+  origin: "bottom", //animations start from bottom
+  distance: "50px", //moves 50px
+  duration: 1000,
+};
+
+//applies scrollreveal animation to head
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",//overrides, slide in from the right
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content h2", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".header__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".jump__image img", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+ScrollReveal().reveal(".jump__content .section__header", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".jump__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".jump__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".service__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".inspire__content .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".inspire__content p", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".mentor__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+
+
+//displays random quotes
+const quotes = [
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "Act as if what you do makes a difference. It does. - William James",
+    "It always seems impossible until it’s done. - Nelson Mandela",
+    "Push through the pain. Giving up hurts more.” — Vegeta, Dragon Ball Z",
+    "Hard work is worthless for those that don’t believe in themselves.” — Naruto Uzumaki, Naruto",
+    "It’s not dying that frightens us. It’s living without ever having done our best.” — The Elder, Castlevania",
+    "Whether You Win Or Lose...You Can Always Come Out Ahead By Learning From The Experience. - All Might"
+]
+
+function getRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  document.getElementById("generateQuote").innerText = quotes[randomIndex];//changes innertext to the actual text
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  getRandomQuote(); // Show a random quote on page load
+  setInterval(getRandomQuote, 8000);
+});
 };
 
 function loadLogin(){
