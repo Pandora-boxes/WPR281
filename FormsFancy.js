01553d0e5dd41a4fb227e05030e5c3f2cadc3423
Cimannin <viljoensteenkamp2004@gmail.com>
2025-02-23 12:24:24 +0200

    forms made, just need some tweaking

---
+++ FormsFancy.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".form-container").classList.add("show");
+});
+//want to let the forms diplay on top of the page when they click to add
